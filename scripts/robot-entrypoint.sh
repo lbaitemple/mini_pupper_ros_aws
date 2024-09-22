476a38450da7d118ac44558bb4ab2c3bf3203726
Li Bai <lbai@temple.edu>
2024-09-22 18:56:14 -0400

    Update robot-entrypoint.sh

--- scripts/robot-entrypoint.sh
+++ scripts/robot-entrypoint.sh
@@ -2,7 +2,7 @@
 
 source /opt/ros/humble/setup.bash
 source /opt/ros_demos/setup.bash
-#source /opt/greengress_bridge/setup.bash
+source /opt/greengress_bridge/setup.bash
 #source /opt/greengrass_bridge/setup.bash
 
 export PYTHONPATH=$PYTHONPATH:$DANCE_CONFIG:/usr/local/lib/python3.10/dist-packages
