18d51a0361fb7c05ffa55bb667a21e829c7bd9fa
Li Bai <lbai@temple.edu>
2024-10-27 12:32:35 -0400

    Update dance.launch.py

--- mini_pupper_dance/launch/dance.launch.py
+++ mini_pupper_dance/launch/dance.launch.py
@@ -3,6 +3,20 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+
+    dance_config_path = os.path.join(
+        get_package_share_directory('mini_pupper_dance'),
+        'routines'
+    )
+
+    bringup_path = os.path.join(
+        get_package_share_directory('mini_pupper_bringup'),
+        'launch/bringup.launch.py'
+    )
+    env = SetEnvironmentVariable(
+            'PYTHONPATH',
+            os.pathsep.join([dance_config_path, os.environ.get('PYTHONPATH', '')])
+    )
     dance_server_node = Node(
             package="mini_pupper_dance",
             namespace="",
@@ -21,8 +35,24 @@ def generate_launch_description():
             executable="pose_controller",
             name="pose_controller",
         )
+    music_server_node = Node(
+            package="mini_pupper_music",
+            namespace="",
+            executable="service",
+            name="music_server",
+            parameters=[
+                {"music_config_path": dance_config_path}
+           ]
+        )
+        
+    bringup_node=IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(bringup_path)
+    )
     return LaunchDescription([
         dance_server_node,
         dance_client_node,
+        env,
+        bringup_node, 
+        music_server_node,
         pose_controller_node
     ])
