061acb431f9ecb4b17135690b438a45042c18467
Li Bai <lbai@temple.edu>
2024-10-27 17:51:53 -0400

    Update dance.launch.py

--- mini_pupper_dance/launch/dance.launch.py
+++ mini_pupper_dance/launch/dance.launch.py
@@ -1,5 +1,10 @@
 from launch import LaunchDescription
+import os
+from launch.actions import SetEnvironmentVariable
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
